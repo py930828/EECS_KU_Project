91b953c4a0e7055600af26ea5463230b58e5aa44
py930828 <cl930828@gmail.com>
2018-05-12 17:04:08 -0500

    Add files via upload

---
+++ EECS_168_Java_Programming/Chen Long -2774386-Lab-01/Exercise2.java
@@ -0,0 +1,14 @@
+public class Exercise2{
+
+	public static void main(String[] args)
+{
+System.out.println("My name is Chen Long");
+System.out.println("I am an EECS major");
+System.out.println("My hobbies are:");
+System.out.println("\t video games");
+System.out.println("\t playing basketball");
+System.out.println("\t watching movies");
+System.out.println("Goodbye");
+System.out.println("my name is chen");
+}
+}
---
+++ EECS_168_Java_Programming/Chen Long -2774386-Lab-01/HelloJava.java
@@ -0,0 +1,9 @@
+public class HelloJava{
+
+	public static void main(String[] args)
+{
+System.out.println("Hello Jave");
+System.out.println("This is going to be a fun lab - I hope!!");
+System.out.println("\t Java!");
+}
+}
